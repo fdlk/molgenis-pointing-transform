f00b4c3b7ccb09956e5f7883446461da60de6574
connor <c.stroomberg@gmail.com>
2019-09-02 12:52:53 +0200

    feat: read studyEvent definition lines into struct

--- scripts/HeaderData.py
+++ scripts/HeaderData.py
@@ -4,4 +4,16 @@ class HeaderData:
         self.nHeaderRows = 0
         self.nStudyEventsDefs = 0
         self.nSubjects = 0
-        self.StudyEvents = []
+        self.studyEvents = []
+
+class StudyEvent:
+    def __init__(self):
+        self.name = ''
+        self.key = '' # En for example: E1 or E12 
+        self.caseReportFroms = []
+
+class CaseReportFrom:
+    def __init__(self):
+        self.name = ''
+        self.version = 0.0
+        self.key = '' # En for example: E1 or E12 
--- scripts/pointing-repeat-table.py
+++ scripts/pointing-repeat-table.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd 
 
 from HeaderData import HeaderData
+from HeaderData import StudyEvent
 
 fileName = 'TAB_test_mogenis_2_2019-08-14-160715176 5.tsv'
 fileFolder = '/Users/connor/Documents/Projecten/Pointing/sprint141/'
@@ -18,10 +19,17 @@ with open(filePath, 'r') as inputFile:
        header.nHeaderRows += 1
        if line.startswith('Study Name:'):
             header.studyName = line[11:].strip()
-       if line.startswith('Study Event Definitions'):
+       elif line.startswith('Study Event Definitions'):
             header.nStudyEventsDefs = line[len('Study Event Definitions'):].strip()
-       if line.startswith('Subjects:'):
+       elif line.startswith('Subjects:'):
             header.nSubjects = line[len('Subjects:'):].strip()
+       elif line.startswith('Study Event Definition'):
+            eventDefString = line[len('Study Event Definition'):].strip()
+            parts = eventDefString.split() # split on tab
+            studyEvent = StudyEvent()
+            studyEvent.name = str(' '.join(parts[1:-1]))
+            studyEvent.key = str(parts[-1])
+            header.studyEvents.append(studyEvent)
        if line in ['\n', '\r\n']:  # check for empty line indicating end of header
             break
 
@@ -31,3 +39,6 @@ print('Number of headerLines: ' + str(header.nHeaderRows))
 print('Study name: ' + str(header.studyName))
 print('Study Event Definitions: ' + str(header.nStudyEventsDefs))
 print('Number of subjects: ' + str(header.nSubjects))
+
+for studyEvent in header.studyEvents:
+    print(studyEvent.name + ' : ' + studyEvent.key)
